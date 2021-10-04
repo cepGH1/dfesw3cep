26790697dea4232c46fd01ce601f9dca00054aba
clare <clarepritchard@hotmail.com>
2021-10-04 11:02:14 +0100

    new file conditionals

---
+++ conditionals.py
@@ -0,0 +1,18 @@
+devs_money = 100
+dev_can_play_smash = False
+if devs_money > 10 and dev_can_play_smash:
+    print("Dev enters a smash tournament")
+elif devs_money < 10 and dev_can_play_smash:
+    print("Dev is too poor to enter")
+else:
+    print("Dev just can't play smash")
+
+myMark = input("please enter your mark: ")
+if int(myMark) > 85:
+    print("distinction")
+elif int(myMark) >= 65:
+    print("pass")
+else:
+    print("fail")
+
+
--- pyTry.py
+++ pyTry.py
@@ -1,2 +1,15 @@
-print("hello python")
-print("I can python")
+#print("hello python")
+#print("I can python")
+#print("this is another string")
+#myVar1 = "My first variable"
+#print(myVar1)
+myInput1 = float(input("please enter a number : "))
+myInput2 = float(input("please enter another number: "))
+answer = myInput1 + myInput2
+
+print(myInput1, "+",  myInput2, "=", str(answer) )
+
+
+
+
+
