3eb2bdd0785007d8848c107235f75a18cd251b4d
clare <clarepritchard@hotmail.com>
2021-10-06 00:04:50 +0100

    exercise 8

---
+++ pythonExercises/pythEx21.py
@@ -0,0 +1,10 @@
+#21 Write a Python program that invoke a given function after specific milliseconds.
+
+import time
+import timeit
+
+myTester = "great"
+test2 = myTester.replace("a", "e")
+print(test2)
+
+
